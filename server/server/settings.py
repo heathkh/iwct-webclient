61b226306ed77da6020509abb7597e337ed6e7bb
ubuntu <ubuntu@ip-10-66-213-118.ec2.internal>
2013-06-22 19:57:22 +0000

    updates

--- server/server/settings.py
+++ server/server/settings.py
@@ -23,7 +23,7 @@ LOGIN_REDIRECT_URL = '/workstations'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '/home/ubuntu/tmp/db.sqlite3',  # Or path to database file if using sqlite3.        
+        'NAME': '/tmp/test_db.sqlite3',  # Or path to database file if using sqlite3.        
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
